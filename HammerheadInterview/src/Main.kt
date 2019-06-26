16b7953c408d9e44217fedd657b5f9cea5d55ce6
Kyle <krober34@students.towson.edu>
2019-06-26 16:48:42 -0400

    Shortened some functions

--- HammerheadInterview/src/Main.kt
+++ HammerheadInterview/src/Main.kt
@@ -4,25 +4,28 @@ import kotlin.system.measureTimeMillis
 fun main()
 {
         println(measureTimeMillis {
-        val services = Services()
-        println(services.komoot.userList.toString())
-
-        println(services.strava.userList.toString())
-
-        println(services.rwgps.userList.toString())
-
-        println("All routes: " + services.getAllRoutes().toString())
-
-        println("Unique routes: " + services.getUniqueRoutes().toString())
-        println()
-
-        val list = mutableListOf("Strava", "Komoot")
-        for (i in 1..5)
-        {
-            val rng = Random.nextInt(1, 100)
-            println("For user $rng: " + services.getAllUserRoutes(rng))
-            println("For user $rng services $list: " + services.getRoutesbyService(rng, list))
+            val services = Services()
+            println("Komoot's user list: " + services.komoot.userList.toString())
             println()
-        }
-    }.toString() + " milliseconds.")
+            println("Strava's user list " + services.strava.userList.toString())
+            println()
+            println("RWGPS's user list " + services.rwgps.userList.toString())
+            println()
+
+            println("All routes: " + services.getAllRoutes().toString())
+
+
+
+            println("Unique routes: " + services.getUniqueRoutes().toString())
+            println()
+
+            val list = mutableListOf("Strava", "Komoot")
+            for (i in 1..5)
+            {
+                val rng = Random.nextInt(1, 100)
+                println("For user $rng: " + services.getAllUserRoutes(rng))
+                println("For user $rng services $list: " + services.getRoutesbyService(rng, list))
+                println()
+            }
+        }.toString() + " milliseconds to complete.")
 }
--- HammerheadInterview/src/Services.kt
+++ HammerheadInterview/src/Services.kt
@@ -2,18 +2,13 @@ import java.lang.IllegalArgumentException
 
 class Services : IServices
 {
-
     val strava = Strava()
     val rwgps = Rwgps()
     val komoot = Komoot()
 
     override fun getAllRoutes() : List<String>
     {
-        val list = mutableListOf<String>()
-        list.addAll(strava.getAllRoutes())
-        list.addAll(rwgps.getAllRoutes())
-        list.addAll(komoot.getAllRoutes())
-        return list
+        return listOf(strava.getAllRoutes(), rwgps.getAllRoutes(), komoot.getAllRoutes()).flatten()
     }
 
     override fun getUniqueRoutes() : List<String>
@@ -23,26 +18,21 @@ class Services : IServices
 
     override fun getAllUserRoutes(userID: Int) : List<String>
     {
-        val list = mutableListOf<String>()
-        list.addAll(strava.getAllUserRoutes(userID))
-        list.addAll(rwgps.getAllUserRoutes(userID))
-        list.addAll(komoot.getAllUserRoutes(userID))
-        return list
+        return listOf(strava.getAllUserRoutes(userID), rwgps.getAllUserRoutes(userID), komoot.getAllUserRoutes(userID)).flatten()
     }
 
     override fun getRoutesbyService(userID: Int, serviceList: List<String>) : List<String>
     {
         val list = mutableListOf<String>()
-        for(service in serviceList)
+
+        serviceList.map { when(it)
         {
-            when(service)
-            {
-                "Strava" -> list.addAll(strava.getAllUserRoutes(userID))
-                "RWGPS" -> list.addAll(rwgps.getAllUserRoutes(userID))
-                "Komoot" -> list.addAll(komoot.getAllUserRoutes(userID))
-                else -> errorResponse("No such service.")
-            }
-        }
+            "Strava" -> list.addAll(strava.getAllUserRoutes(userID))
+            "RWGPS" -> list.addAll(rwgps.getAllUserRoutes(userID))
+            "Komoot" -> list.addAll(komoot.getAllUserRoutes(userID))
+            else -> errorResponse("No such service.")
+        } }
+
         return list
     }
 
